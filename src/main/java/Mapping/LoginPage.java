b12ae1f31d8612066efb233a21c3fa53629adb44
Lidor Sabag <lidor.sabagx@gmail.com>
2022-11-21 19:05:41 +0200

    Adding 2 Mapping + 1 Test class

---
+++ src/main/java/Mapping/LoginPage.java
@@ -0,0 +1,18 @@
+package Mapping;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+    WebDriver driver;
+
+    @FindBy (xpath = "//*[@id=\'app\']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")
+    public static WebElement UserNameField;
+
+    @FindBy (xpath = "//*[@id=\'app\']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")
+    public static WebElement PasswordField;
+
+    @FindBy (xpath = "//*[@id=\'app\']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
+    public static WebElement SignInButton;
+}
---
+++ src/main/java/TestCases/LoginPageTestCases.java
@@ -0,0 +1,9 @@
+package TestCases;
+
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPageTestCases extends BaseDriver{
+    public void LoginPageStart (){
+        PageFactory.initElements(driver,);
+    }
+}
